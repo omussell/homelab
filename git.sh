0cdb7a2d7eae2116e6f2d33ae5f72689c3ab725d
oem <oliver.mussell@hotmail.co.uk>
2017-01-29 17:58:15 +0000

    Updated index.html

--- git.sh
+++ git.sh
@@ -1,14 +1,8 @@
 PWD=`pwd`
-<<<<<<< HEAD
 pandoc -o ~/www/index.html ~/www/index.md
 pandoc -s -S -c /design/style.css --toc ~/www/design/designdoc.txt -o ~/www/design/index.html
 cd ~/www
-=======
-pandoc -o ~/www/homelab/index.html ~/www/homelab/index.md
-pandoc -s -S -c /homelab/design/style.css --toc ~/www/homelab/design/designdoc.txt -o ~/www/homelab/design/index.html
-cd ~/www/homelab
->>>>>>> 75ca7bc9611273ab6ae01d0e1502dea51598f599
 git add --all
 git commit -m "Updated index.html"
-git push -u origin gh-pages
+git push origin master
 cd $PWD
